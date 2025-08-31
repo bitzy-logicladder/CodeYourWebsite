aec3c8bfd555a20513a367c33f1852f3fe70d3af
bitzy-logicladder <bitzy.logicladder@gmail.com>
2025-08-31 19:53:04 +0100

    Part 22 - DOM Basics
    Part 22 - DOM Basics

---
+++ Part22-DOMBasics/MyWebsite/script.js
@@ -0,0 +1,7 @@
+alert("Welcome to my portfolio Website! I am inside an external JS file!");
+
+
+document.getElementsByTagName("body")[0].style.backgroundImage="URL('images/bg3.jpg')";
+
+document.getElementById("note").innerHTML = "I am changed using external javascript inner HTML property.";
+
